b1d8a6e52586bc4d246ecd5e082a2a3c91be52c6
davidauza.engineer <davidauza01@gmail.com>
2019-10-05 10:29:49 -0500

    Create displayCoins method

--- app/src/main/java/engineer/davidauza/saveapplication/MainActivity.java
+++ app/src/main/java/engineer/davidauza/saveapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     int coins;
@@ -24,20 +26,27 @@ public class MainActivity extends AppCompatActivity {
         coins = settings.getInt("coins", coins);
 
         coinsTextView = findViewById(R.id.coins_text_view);
-        coinsTextView.setText("Coins: " + Integer.toString(coins));
+        displayCoins();
     }
 
     /**
      * This method add 500 coins and saves the changes
-     *
-     * @param pView
      */
     public void addCoins(View pView) {
         coins += 500;
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putInt("coins", coins);
-        editor.commit();
-        coinsTextView.setText("Coins: " + Integer.toString(coins));
+        editor.apply();
+        displayCoins();
+    }
+
+    /**
+     * This method display the coins in its corresponding TextView.
+     */
+    private void displayCoins() {
+        String totalCoins = getString(R.string.total_coins) + " " +
+                String.format(Locale.getDefault(), "%d", coins);
+        coinsTextView.setText(totalCoins);
     }
 }
